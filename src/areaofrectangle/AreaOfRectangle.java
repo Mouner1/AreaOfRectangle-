f404039555ad4edd59838c12e64aca11837666f5
modab5310 <modab5310@7HJM6Y1.Student.UGDSB.ED>
2018-10-09 09:19:52 -0400

    Java

---
+++ src/areaofrectangle/AreaOfRectangle.java
@@ -0,0 +1,30 @@
+/*
+ * This page was created by Mouner Dabjab
+ * on September 20
+ * To calculate the area of a rectangle 
+ */
+
+package areaofrectangle;
+
+/**
+ *
+ * @author modab5310
+ */
+public class AreaOfRectangle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double length = 5.7;
+        double width = 4.8;
+        double area = 0.0;
+        
+        // algorithm for area calc
+        area= length*width;
+        System.out.println("the area is"+ area + "cm squared");
+        System.out.println("Width= 4.8 \n length= 5.7");
+             
+    }
+    
+}
